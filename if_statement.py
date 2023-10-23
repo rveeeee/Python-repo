93307743d68f1aed1b3ddebd082dff9074dc09f9
Rv <rvebreo777@gmail.com>
2023-10-24 00:27:28 +0800

    if statements
    Comparison if statements

---
+++ if_statement.py
@@ -0,0 +1,10 @@
+house= 1_000_000
+
+good_credit = False
+
+if good_credit:
+    down_payment = house * 0.1
+else:
+    down_payment = house * 0.2
+
+print(f"Your downpayment is: ${down_payment} ")
---
+++ if_statement_comparison.py
@@ -0,0 +1,12 @@
+name = input("What is your name: ")
+
+
+if len(name) < 3:
+    print('Name must be at least 3 characters')
+elif len(name) > 50:
+    print ('Name can be a maximum of 50 characters')
+else:
+    print("Name looks good!")
+print(len(name)) # I added the len function here to visualize what it does
+# My error here is that in the first try i didn't use the len function at first which results to an error.
+# I forgot that the name string cannot be compared to an integer, so I need to use len in order to compare these two.

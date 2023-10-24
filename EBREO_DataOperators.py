40c839bcd3034977a10e74e10a66d1aa3f93fc6d
Rv <rvebreo777@gmail.com>
2023-10-24 13:46:10 +0800

    Data Operators
    Activity Lab in OOP on October 24, 2023

---
+++ EBREO_DataOperators.py
@@ -0,0 +1,14 @@
+first_number = float(input('First Number: '))
+second_number = float(input('Second Number: '))
+
+sum = first_number + second_number
+print (f"The sum of two numbers is: {sum}")
+
+difference = first_number - second_number
+print (f"The difference of two numbers is: {difference}")
+
+product = first_number * second_number
+print (f"The product of two numbers is: {product}")
+
+quotient = first_number / second_number
+print (f"The quotient of the two number is: {quotient}")
